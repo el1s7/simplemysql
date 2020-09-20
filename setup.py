b42f33ca0243ad420bafe8aef919eb763851bff3
elis-k <me@elis.cc>
2020-09-20 20:34:16 +0200

    Python3 Compatible

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+	name="simplemysql",
+	version="1.25",
+	description="An ultra simple wrapper for Python MySQLdb with very basic functionality",
+	author="Kailash Nadh",
+	author_email="kailash.nadh@gmail.com",
+	url="http://nadh.in/code/simplemysql",
+	packages=['simplemysql'],
+	download_url="http://github.com/knadh/simplemysql",
+	license="GPLv2",
+	classifiers=[
+		"Development Status :: 3 - Alpha",
+		"Intended Audience :: Developers",
+		"Programming Language :: Python",
+		"Natural Language :: English",
+		"License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+		"Programming Language :: Python :: 3.5",
+		"Topic :: Software Development :: Libraries :: Python Modules",
+		"Topic :: Database",
+		"Topic :: Software Development :: Libraries"
+	],
+	install_requires=["mysqlclient"]
+)
