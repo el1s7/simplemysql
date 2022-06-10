bc0c28b5fc06b3ed157e0e7834b7d2f417fb2db4
Eli$ <github@elis.cc>
2022-06-10 19:27:39 +0200

    Add mysql_escape_string
    mysqlclient has decided to remove escape_string function for no reason...

--- simplemysql/helpers.py
+++ simplemysql/helpers.py
@@ -1,3 +1,5 @@
+import re
+
 def run_once(f):
 	def wrapper(*args, **kwargs):
 		if not wrapper.has_run:
@@ -5,3 +7,24 @@ def run_once(f):
 			return f(*args, **kwargs)
 	wrapper.has_run = False
 	return wrapper
+
+def _mysql_escape_characters(match):
+    char = match.group()
+
+    escapes = {
+        "\0": "\\0",
+        "\x08": "\\b",
+        "\x09": "\\t",
+        "\x1a": "\\z",
+        "\n": "\\n",
+        "\r": "\\r",
+        "\"": "\\\"",
+        "'": "\\'",
+        "\\": "\\\\",
+        "%": "\\%"
+    }
+
+    return escapes.get(char, char)
+
+def mysql_escape_string(string):
+    return re.sub(r"[\0\x08\x09\x1a\n\r\"\'\\\%]", _mysql_escape_characters, string)
